8527d2faff1fbf4a185f96a978fdb6ba03d4cb18
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2020-08-15 05:31:53 +0100

    new commit

---
+++ algo.js
@@ -0,0 +1,21 @@
+
+
+
+function caeserCipher(string, shift) {
+    const alphabetArr = "abcdefghijklmnopqrstuvwxyz".split("")
+    let result = ""
+    for (let i = 0; i < string.length; i++) {
+        const currentChar = string[i]
+        const idx = alphabetArr.indexOf(currentChar)
+        if (idx === -1) {
+            result += currentChar
+            continue;
+        }
+
+        const encodedIdx = (idx + shift) % 26
+        res += alphabetArr[encodedIdx]
+
+    }
+
+    return res
+}
---
+++ testRave.js
@@ -0,0 +1,36 @@
+var Ravepay = require('flutterwave-node');
+ 
+var rave = new Ravepay("FLWPUBK_TEST-b5440b6a7b601887fbe55ac82d5d6304-X", "FLWSECK_TEST-9f158f5ea8116153c1e38e713c9415dd-X", false);
+ 
+rave.Card.charge(
+    {
+        "cardno": "5438898014560229",
+        "cvv": "564",
+        "expirymonth": "10",
+        "expiryyear": "20",
+        "currency": "NGN",
+        "country": "NG",
+        "amount": "10000",
+        "email": "user@gmail.com",
+        "phonenumber": "0902620185",
+        "firstname": "temi",
+        "lastname": "desola",
+        "IP": "355426087298442",
+        "txRef": "MC-" + Date.now(),// your unique merchant reference
+        "meta": [{metaname: "flightID", metavalue: "123949494DC"}],
+        "redirect_url": "https://rave-webhook.herokuapp.com/receivepayment",
+        "device_fingerprint": "69e6b7f0b72037aa8428b70fbe03986c"
+      }
+).then(resp => {
+    rave.Card.validate({
+        "transaction_reference":resp.body.data.flwRef,
+        "otp":12345
+    }).then(response => {
+        console.log(response.body.data.tx,"checked");
+        
+    })
+    
+}).catch(err => {
+    console.log(err);
+    
+})
